cc5372e15d7448d0b299588ada991d6c4b7dbed8
Fazle Rabbi <you@example.com>
2022-05-09 12:57:07 +0600

    async added

---
+++ index.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config();
+const port = process.env.PORT || 5000;
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.p5e7i.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        const productsCollection = client.db('abacusElectronics').collection('products');
+
+        app.get('/products', async (req, res) => {
+            const query = {};
+            const cursor = productsCollection.find(query);
+            const products = await cursor.toArray();
+            res.send(products);
+        });
+
+        app.get('/products/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const product = await productsCollection.findOne(query);
+            res.send(product);
+        })
+
+    }
+    finally {
+
+    }
+
+}
+
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+    res.send('WORKING')
+});
+
+app.listen(port, () => {
+    console.log('Listening to port', port)
+});
